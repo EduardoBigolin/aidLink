deaadd72412cdea3304e0dfc79a6c120121da481
Lucas Zorrer <lucaszorrer10@outlook.com>
2024-05-19 00:06:14 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
-import Express from "express";
-import router from "./routes/route";
-import cors from "cors";
+import Express from 'express'
+import router from './routes/route'
+import cors from 'cors'
 
-const app = Express();
-app.use(
-  cors({
-    origin: "https://localhost:5173/", // Permite apenas requisições vindas deste endereço
-    credentials: true, // Se você estiver usando cookies com credenciais, isso é necessário
-  })
-);
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }));
-app.use(Express.static("public"));
-app.use(router);
+const app = Express()
+app.use(cors())
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
+app.use(Express.static('public'))
+app.use(router)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3000");
-});
+  console.log('Server running on port 3000')
+})
